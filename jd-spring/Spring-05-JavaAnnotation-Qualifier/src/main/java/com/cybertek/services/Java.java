ef0f2cb38f6bf8844ecf5a69d554c6dd39d709f7
“xiayimard <karwanghana@gmail.com>
2022-09-29 23:27:52 +0100

    Spring 05 java annotation qualifier

--- jd-spring/Spring-05-JavaAnnotation-Qualifier/src/main/java/com/cybertek/services/Java.java
+++ jd-spring/Spring-05-JavaAnnotation-Qualifier/src/main/java/com/cybertek/services/Java.java
@@ -3,15 +3,18 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
-
+//    @Autowired
+//    @Qualifier("officeHours")
     private ExtraSession extraSession;
-@Autowired
-    public Java(ExtraSession extraSession) {
+
+    // The other ay to qualifier
+    public Java(@Qualifier("officeHours") ExtraSession extraSession) {
         this.extraSession = extraSession;
     }
 
