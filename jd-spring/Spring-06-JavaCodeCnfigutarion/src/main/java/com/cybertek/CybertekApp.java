54ff176b6ded61e9b4c783919a63f6906ccb93b6
“xiayimard <karwanghana@gmail.com>
2022-09-30 00:16:07 +0100

    Spring 05 java code config

---
+++ jd-spring/Spring-06-JavaCodeCnfigutarion/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,22 @@
+package com.cybertek;
+
+
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.cybertek.configs.CybertekAppConfig;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext container= new AnnotationConfigApplicationContext(CybertekAppConfig.class);
+
+        Course course=container.getBean("java",Course.class);
+
+        course.getTeachHours();
+
+    }
+}
