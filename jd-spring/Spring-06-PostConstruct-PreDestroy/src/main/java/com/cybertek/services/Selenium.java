a2931a7cc0ae2a7a5f450eb1cc23d243bad94e41
“xiayimard <karwanghana@gmail.com>
2022-09-30 01:05:47 +0100

    Spring 06 post cunstruct pre destroy

---
+++ jd-spring/Spring-06-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
@@ -0,0 +1,15 @@
+package com.cybertek.services;
+
+
+import com.cybertek.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium implements Course {
+    @Override
+    public void getTeachHours() {
+
+
+        System.out.println(" Weekly teaching hours : 15" );
+    }
+}
