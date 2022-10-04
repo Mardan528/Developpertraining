10a2a5ae7a51c5a91a1c02e5cdeb147a0a40aeb8
“xiayimard <karwanghana@gmail.com>
2022-10-04 16:48:23 +0100

    Spring  boot lab

---
+++ jd-spring/Spring-BootLab/src/main/java/com/cybertek/services/carpet/CarpetTX.java
@@ -0,0 +1,35 @@
+package com.cybertek.services.carpet;
+
+import com.cybertek.enums.City;
+import com.cybertek.interfaces.carpetPrices.Carpet;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class CarpetTX implements Carpet {
+
+    private static final Map<City, BigDecimal> spPriceForCity=new HashMap<>();
+
+    static {
+
+        spPriceForCity.put(City.AUSTIN,new BigDecimal("4.32"));
+        spPriceForCity.put(City.DULLES,new BigDecimal("2.92"));
+        spPriceForCity.put(City.SAN_ANTONIO,new BigDecimal("5.72"));
+
+    }
+
+
+    @Override
+    public BigDecimal getSqFtPrice(City city) {
+
+        BigDecimal defaultValue=BigDecimal.ZERO;
+
+
+        Optional<Map.Entry<City,BigDecimal>> collect=spPriceForCity.entrySet().stream().filter(x-> x.getKey()==city).findFirst();
+
+
+        return collect.isPresent() ? collect.get().getValue() : defaultValue;
+    }
+}
---
+++ jd-spring/Spring-BootLab/src/main/java/com/cybertek/services/carpet/CarpetVA.java
@@ -0,0 +1,36 @@
+package com.cybertek.services.carpet;
+
+import com.cybertek.enums.City;
+import com.cybertek.interfaces.carpetPrices.Carpet;
+import org.springframework.cglib.beans.BeanMap;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class CarpetVA implements Carpet {
+
+    private static final Map<City,BigDecimal> spPriceForCity=new HashMap<>();
+
+    static {
+
+        spPriceForCity.put(City.MACLEAN,new BigDecimal("4.32"));
+        spPriceForCity.put(City.ARLINGTON,new BigDecimal("2.92"));
+        spPriceForCity.put(City.AUSTIN,new BigDecimal("5.72"));
+
+    }
+
+
+    @Override
+    public BigDecimal getSqFtPrice(City city) {
+
+        BigDecimal defaultValue=BigDecimal.ZERO;
+
+
+      Optional<Map.Entry<City,BigDecimal>> collect=spPriceForCity.entrySet().stream().filter(x-> x.getKey()==city).findFirst();
+
+
+        return collect.isPresent() ? collect.get().getValue() : defaultValue;
+    }
+}
